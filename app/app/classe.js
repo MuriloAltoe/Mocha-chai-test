83d7eadc0f85e1ee8a7aac80791df057f8508412
Murilo Altoé <muriloaleme17@gmail.com>
2023-10-12 18:42:40 -0300

    1st-commit

---
+++ app/app/classe.js
@@ -0,0 +1,25 @@
+
+class Classe {
+    soma(n1, n2) {
+        var n3;
+        return n3 = n1 + n2;
+    };
+
+    subtração(n1, n2) {
+        var n3;
+        return n3 = n1 - n2;
+    }
+
+    multiplicacao(n1, n2) {
+        var n3;
+        return n3 = n1 * n2;
+    };
+
+    divisao(n1, n2) {
+        var n3;
+        return n3 = n1 / n2;
+    }
+};
+
+module.exports = Classe;
+
---
+++ app/specs/classe.spec.js
@@ -0,0 +1,23 @@
+
+const chai = require('chai');
+const Classe = require('../app/classe');
+
+const calc = new Classe()
+
+describe('Teste da classe', () => {
+    it('Testar a soma', () => {
+        chai.expect(calc.soma(5, 2)).to.equal(7);
+    });
+
+    it('Testar a subtração', () => {
+        chai.expect(calc.subtração(5, 2)).to.equal(3);
+    });
+
+    it('Testar a multiplicação', () => {
+        chai.expect(calc.multiplicacao(5,2)).to.equal(10);
+    });
+
+    it('Testar a divisão', () => {
+        chai.expect(calc.divisao(5, 2)).to.equal(2.5);
+    });
+});
